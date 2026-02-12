06a133a3b0eef267e69e8effaddb375a097442a4
carlosandresflorezvargas2007@gmail.com <B15S201est@MEDAULAS.UPB>
2026-02-12 14:29:38 -0500

    chore: initial project structure

---
+++ src/test/java/org/example/taller1mvc/Taller1mvcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.taller1mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taller1mvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
